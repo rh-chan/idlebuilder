ec81ba428ad48b8d2fcd48220f6f52a90fffda40
rh-chan <rh.chan@mail.utoronto.ca>
2018-08-30 12:44:58 -0400

    finished beta

--- app/idlesummoner/views.py
+++ app/idlesummoner/views.py
@@ -10,10 +10,12 @@ from idlesummoner.key import FORTNITE_TOKEN, LAUNCHER_TOKEN, PASSWORD, EMAIL
 #fortnite = Fortnite(fortnite_token=FORTNITE_TOKEN, launcher_token=LAUNCHER_TOKEN, password=PASSWORD, email=EMAIL)
 
 def get_fortnite_id(username):
-    #player = fortnite.player(username)
-    #id = player.id
+    player = fortnite.player(username)
+    id = player.id
+    if id is None:
+        id = -1
 
-    id = "00000000"
+    #id = "00000000"
     return id
 
 class FortniteNameView(APIView):
